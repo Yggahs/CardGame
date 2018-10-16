88c7a72bd6cccc037b9e62079d634b8c3eae02a7
Alessio <alessio.calore96@gmail.com>
2018-10-16 01:32:12 +0100

    Base Hand andField Interaction, Button and Phases
    Cards in hand panel can be moved around and dropped on the battlefield;
    Pressing the button will change the phase.
    Known Bugs: NullReferenceException: Object reference not set to an instance of an object
    Manager.ChangeText (System.String word) (at Assets/Scripts/Manager.cs:75)
    Manager.Start () (at Assets/Scripts/Manager.cs:27)

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+    bool DrawPhase = false;
+    bool MainPhase1 = false;
+    bool BattlePhase = false;
+    bool MainPhase2 = false;
+    bool EndPhase = false;
+    string currentPhase;
+
+    public Text currentText = null;
+
+
+	// Use this for initialization
+	void Awake () {
+        
+        DrawPhase = true;
+        currentPhase = "DrawPhase";
+        
+    }
+
+    private void Start()
+    {
+        ChangeText(currentPhase);
+    }
+    public void ChangePhase()
+    {
+        
+        ChangeText(currentPhase);
+        if (DrawPhase && !MainPhase1 && !BattlePhase && !MainPhase2 && !EndPhase)
+        {
+            DrawPhase = false;
+            MainPhase1 = true;
+            currentPhase = "MainPhase1";
+            
+        }
+        else if (!DrawPhase && MainPhase1 && !BattlePhase && !MainPhase2 && !EndPhase)
+        {
+            MainPhase1 = false;
+            BattlePhase = true;
+            currentPhase = "BattlePhase";
+            
+        }
+        else if (!DrawPhase && !MainPhase1 && BattlePhase && !MainPhase2 && !EndPhase)
+        {
+            BattlePhase = false;
+            MainPhase2 = true;
+            currentPhase = "MainPhase2";
+            
+        }
+        else if (!DrawPhase && !MainPhase1 && !BattlePhase && MainPhase2 && !EndPhase)
+        {
+            MainPhase2 = false;
+            EndPhase = true;
+            currentPhase = "EndPhase";
+            
+        }
+        else if(!DrawPhase && !MainPhase1 && !BattlePhase && !MainPhase2 && EndPhase)
+        {
+            EndPhase = false;
+            currentPhase = "Not your turn!";
+        }
+        else
+        {
+            DrawPhase = true;
+            currentPhase = "DrawPhase";
+        }
+        
+    }
+    public void ChangeText(string word)
+    {
+        currentText.text = word;
+    }
+}
